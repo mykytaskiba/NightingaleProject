276631ab0cfb040e7cbfde0bc04a14f8641d6ec0
mykytaskiba <skibamykyta@gmail.com>
2026-04-04 02:07:05 -0700

    Update file_source.h

--- NightingaleEnvironment/NightingaleEngine/Code/file_source.h
+++ NightingaleEnvironment/NightingaleEngine/Code/file_source.h
@@ -2,8 +2,7 @@
 #include <filesystem>
 
 struct FileSource {
-	FileSource(std::filesystem::path path) : m_path(path) {
-	}
+	FileSource(std::filesystem::path path) : m_path(path) {}
 
 	std::filesystem::path m_path;
 
